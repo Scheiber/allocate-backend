cd4ed31c824441d1359169c063d85f7b0682a311
Jonathan Scheiber <jon@scheiber.me>
2022-07-25 01:28:10 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
-//Dependencies
-const express = require("express");
-const app = express();
-const cors = require("cors");
-
+// Dependencies
+const app = require("./app.js");
 require("dotenv").config();
 
-//Port
-// Allow use of Heroku's port or your own local port, depending on the environment
+// Port (allow use of Heroku's port or local port, depending on the environment)
 const PORT = process.env.PORT || 3003;
 
-//___________________
-//Middleware
-//___________________
-
-app.use(express.json()); // returns middleware that only parses JSON
-
-// this allows any app/site from anywhere access your API. This is a great way to start to get things up and running. Later, add restrictions, as needed.
-app.use(cors());
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("<h1 style='text-align:center;'>Welcome to Allocate!</h1>");
-});
-
-//Listen
+// Listen
 app.listen(PORT, () => console.log("Listening on port:", PORT));
