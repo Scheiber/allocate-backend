a29418c4ff88f22b7a493e481516aee7532b8a07
Jonathan Scheiber <jon@scheiber.me>
2022-07-25 01:27:58 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-//Dependencies
+// Dependencies
 const express = require("express");
+const cors = require("cors");
+
+// Config
 const app = express();
+
+// Middleware to enable CORS on server
+app.use(cors());
+
+// Middleware that turns JSON into usable JS
+app.use(express.json());
+
+const transactionsController = require("./controllers/transactionsController.js");
+
+// Routes
+app.get("/", (req, res) => {
+  res.send(`<h1 style='text-align:center;'>Welcome to Allocate!</h1>`);
+});
+
+app.use("/transactions", transactionsController);
+
+// Error
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Error 404: Not Found" });
+});
+
+// Export
+module.exports = app;
