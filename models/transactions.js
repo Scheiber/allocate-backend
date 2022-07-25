65bae9c4b29431b175db89d83471bd4da219cedf
Jonathan Scheiber <jon@scheiber.me>
2022-07-25 01:27:38 -0400

    update models

---
+++ models/transactions.js
@@ -0,0 +1,74 @@
+transactions = [
+  {
+    item_name: "Income",
+    amount: 1500,
+    date: "July 1, 2022",
+    from: "Employer",
+    category: "income",
+  },
+  {
+    item_name: "Taxes",
+    amount: -300,
+    date: "July 1, 2022",
+    from: "Department of Finance",
+    category: "expense",
+  },
+  {
+    item_name: "Retirement",
+    amount: -200,
+    date: "July 1, 2022",
+    from: "Fidelity",
+    category: "expense",
+  },
+  {
+    item_name: "Savings",
+    amount: -100,
+    date: "July 1, 2022",
+    from: "TD Bank",
+    category: "expense",
+  },
+  {
+    item_name: "Credit Card Paymemt",
+    amount: -100,
+    date: "July 1, 2022",
+    from: "Capital One",
+    category: "expense",
+  },
+  {
+    item_name: "Birthday Gift",
+    amount: 20,
+    date: "July 3, 2022",
+    from: "Uncle Leo",
+    category: "income",
+  },
+  {
+    item_name: "Coffee",
+    amount: 8,
+    date: "July 3, 2022",
+    from: "Starbucks",
+    category: "expense",
+  },
+  {
+    item_name: "Internet",
+    amount: -90,
+    date: "July 5, 2022",
+    from: "Spectrum",
+    category: "expense",
+  },
+  {
+    item_name: "Groceries",
+    amount: -76,
+    date: "July 5, 2022",
+    from: "Amazon Fresh",
+    category: "expense",
+  },
+  {
+    item_name: "Dog Food",
+    amount: -50,
+    date: "July 7, 2022",
+    from: "PetSmart",
+    category: "expense",
+  },
+];
+
+module.exports = transactions;
